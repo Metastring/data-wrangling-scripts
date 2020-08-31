1c02c85bf0ed6edfec77045b9b95c73901ab6dea
Akshay S Dinesh <asdofindia@gmail.com>
2020-08-31 11:38:40 +0530

    feat: check argument is passed in copy script

--- idsp/copy.sh
+++ idsp/copy.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -z "$1" ]
+  then
+    echo "Pass in the data directory as first argument"
+    exit
+fi
+
 for DIRECTORY in `ls data`; do
     if [ -d "data/${DIRECTORY}/clean" ]
     then
